2606013632a9361413e6fb8d085445bc29d5c28b
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2020-12-03 22:23:29 +0530

    basic structure complete

--- contentScript.js
+++ contentScript.js
@@ -1,32 +1,71 @@
-// console.log(typeof document.body.textContent);
-// document.body.innerHTML = document.body.innerHTML.replace('hello', '昨日');
-
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     console.log(sender.tab ?
-//                 "from a content script:" + sender.tab.url :
-//                 "from the extension");
-//     if (request.greeting == "hello")
-//       console.log('Received the object');
-// });
-
-
-chrome.storage.sync.set({'KEY':'VAL'}, function() {
-  // console.log('Value is set to ' + value);
-});
-
-chrome.storage.sync.clear();
-
-chrome.storage.sync.get(null, function(result) {
-  // console.log('Value currently is ' + result['key']);
-  for(item in result){
-    console.log(item, result[item]);
+// run when page loads
+chrome.storage.sync.get(null, function(wordsDict) {
+  console.log(wordsDict['wordsList']);
+  var parentArray = wordsDict['wordsList'];
+  for(var i = 0; i < parentArray.length; i++){
+    var fromWord = parentArray[i][0], toWord = parentArray[i][1];
+    console.log(fromWord, toWord);
+    // var ful = document.body.innerHTML;
+    console.log(document.title);
+    document.body.innerHTML = document.body.innerHTML.replace(fromWord, toWord);
+    console.log("done");
+    // console.log(ful == document.body.innerHTML);
   }
+  // for(item in wordsDict['wordsList']){
+  //   // fromWord = item[0]
+  //   // toWord = item[1]
+  //   // console.log(fromWord, toWord);
+  //   console.log(JSON.stringify(item[0][0]));
+  //   // document.body.innerHTML = document.body.innerHTML.replace(fromWord, toWord);
+  // }
   // console.log(typeof result.key);
   // console.log(object)
   // console.log(result);
+  // document.body.innerHTML = document.body.innerHTML.replace('extension', '昨');
+  // document.body.innerHTML = "This is it."
+  // console.log(document.body.innerHTML);
 });
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // console.log(request.fromWord, request.toWord);
+    console.log(request.type);
+    if (request.type == "addWords"){
+      addWord(request.fromWord, request.toWord);
+    }
+    else if (request.type == "clear"){
+      clearStorage();
+    }
+    else if (request.type == "displayAll"){
+      displayAll()
+    }
+  });
 
+function addWord(fromWord, toWord) {
+  console.log(fromWord, toWord);
+  // var tempDict = {fromWord: toWord}
+  chrome.storage.sync.set({wordsList: [['hemmer', '昨日'], ['card', '日'], ['extensions', '昨'], ['now', '日']]}, function() {
+    // console.log('Value is set to ' + value);
+  });
+}
 
+function clearStorage() {
+  chrome.storage.sync.clear();
+}
 
+// for debugging
+function displayAll() {
+  // run when page loads
+  console.log("displayAll fired");
+  chrome.storage.sync.get(null, function(wordsDict) {
+    console.log(wordsDict['wordsList']);
+    var parentArray = wordsDict['wordsList'];
+    for(var i = 0; i < parentArray.length; i++){
+      var fromWord = parentArray[i][0], toWord = parentArray[i][1];
+      console.log(fromWord, toWord);
+    }
+  });
+}
+
+// console.log(typeof document.body.textContent);
+// document.body.innerHTML = document.body.innerHTML.replace('online', '昨日');
--- popup.js
+++ popup.js
@@ -1,14 +1,33 @@
-// function sendMessage(event){
-//   event.stopPropagation();
-//   event.preventDefault();
-//   console.log("YES");
-//   // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   //   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-//   //     console.log(response.farewell);
-//   //   });
-//   // });
-// }
+document.addEventListener('DOMContentLoaded', function () {
+  var addBtn = document.getElementById('addWord');
+  var clearBtn = document.getElementById('clearWords');
+  var displayBtn = document.getElementById('displayWords');
 
-// chrome.browserAction.onClicked.addListener(sendMessage);
+  // event listener for adding words button
+  addBtn.addEventListener('click', function() {
+    var fromWord = document.getElementById('fromWord').value;
+    var toWord = document.getElementById('toWord').value;
+    console.log(fromWord, toWord);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: "addWords", fromWord: fromWord, toWord: toWord});
+    });
+  });
 
-console.log('test1');
+  // event listener for cleaning chrome storage
+  clearBtn.addEventListener('click', function() {
+    // send message to contentScipt to clear storage
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: "clear"});
+    });
+  });
+
+  // for debugging
+  displayBtn.addEventListener('click', function() {
+    // send message to contentScipt to clear storage
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: "displayAll"});
+    });
+  });
+
+});
+
